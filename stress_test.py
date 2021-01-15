b6a9e63989fec8de8bafadbae63bca432820b418
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2021-01-15 13:42:01 +0530

    benchmark & stress test
    this is a benchmark test using python script for PC (tested on windows)

---
+++ stress_test.py
@@ -0,0 +1,36 @@
+import psutil
+from time import ctime
+import time
+from math import sin, cos, radians
+import csv
+
+count = 0
+Myfile = open("data.csv",'a')
+printData = csv.writer(Myfile)
+iTime = 0
+fTime = 0
+
+def bench():
+    start = time.time()
+    product = 1.0
+    for counter in range(1, 1000, 1):
+        for dex in list(range(1, 360, 1)):
+            angle = radians(dex)
+            product *= sin(angle)**2 + cos(angle)**2
+    end = time.time()
+    return end - start
+
+iTime = time.time()
+printData.writerow(["Date-Time","hexadecimal","CPU %","Benchmark time","Running From"])
+while True:
+    try:
+        print(hex(count))
+        count += 1
+        hexdata = hex(count)
+        power = bench()
+        fTime = time.time()
+        printData.writerow([ctime(),hexdata,psutil.cpu_percent(),power,fTime - iTime])
+    except KeyboardInterrupt:
+        break
+        
+Myfile.close()
